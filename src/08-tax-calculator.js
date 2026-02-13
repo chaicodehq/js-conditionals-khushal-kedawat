01422b0a2f4b7ca8f7cb975433d030ac0c4d1c63
khushal <khushal.kedawat@gmail.com>
2026-02-14 00:41:39 +0530

    08-tax done

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,19 @@
  */
 export function calculateTax(income) {
   // Your code here
+  let tax = 0
+  if(income <= 10000)return 0
+  if(income > 10000 && income <= 30000){
+    tax = 0.1*(income - 10000)
+    return tax
+  }else if(income > 30000 && income <= 70000){
+    tax = 0.2*( income - 30000) + 0.1*(30000 - 10000)
+    return tax
+  }else if(income > 70000){
+    tax += 0.3*(income - 70000) + 0.2*( 70000 - 30000) + 0.1*(30000 - 10000)
+    return tax
+  }
+  
+
+
 }
